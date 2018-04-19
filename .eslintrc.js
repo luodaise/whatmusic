cdde9ca1cb9335b8c6ee16cf5ec4303973503bd0
luodaise <luodaise@icloud.com>
2018-04-19 16:34:19 +0800

    轮播初建 dom操作 url获取

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,20 @@
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  extends: [
+  extends:
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    //'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
-  ],
+  ,
   // required to lint *.vue files
   plugins: [
     'html'
@@ -22,7 +23,8 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'eol-last': 0,
