35e8172be801c55aeece70424cc061fa0a63a504
luodaise <luodaise@icloud.com>
2018-05-03 17:08:44 +0800

    搜索历史组件

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -14,6 +14,12 @@ function insertArray(arr, val, compare, maxLen) {
     arr.pop()
   }
 }
+function deleteFromArray(arr, compare) {
+  const index = arr.findIndex(compare)
+  if (index > -1) {
+    arr.splice(index, 1)
+  }
+}
 export function saveSearch(query) {
   let searches = storage.get(SEARCH_KEY, [])
   insertArray(searches, query, (item) => {
@@ -21,4 +27,19 @@ export function saveSearch(query) {
   }, SEARCH_MAX_LENGTH)
   storage.set(SEARCH_KEY, searches)
   return searches
+}
+export function loadSearch() {
+  return storage.get(SEARCH_KEY, [])
+}
+export function deleteSearch(query) {
+  let searches = storage.get(SEARCH_KEY, [])
+  deleteFromArray(searches, (item) => {
+    return item === query
+  })
+  storage.set(SEARCH_KEY, searches)
+  return searches
+}
+export function clearSearch() {
+  storage.remove(SEARCH_KEY)
+  return []
 }
